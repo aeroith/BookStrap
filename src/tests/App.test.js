9d3412e9dc483287cc05747c7c33850123340964
aeroith <scillidag@gmail.com>
2017-04-13 11:11:47 +0300

    Add react-test-renderer as dependency to fix issue with testing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "../components/App";
 import MenuContainer from "../components/App/MenuContainer";
 import DropdownSearch from "../components/App/customSearch";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 it("renders without crashing", () => {
     shallow(<App />);
     shallow(<MenuContainer />);
